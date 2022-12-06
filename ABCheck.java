9d88c41d91f7ef88c5e1edaf6b5fbea7666438b1
SHUBHANKAR <50442880+ShubhP23@users.noreply.github.com>
2022-12-06 13:43:52 +0530

    Create ABCheck.java
    Program to Check the string value "TRUE" if the characters “a” and “b” are separated by exactly 3 places anywhere in the string at least once.

---
+++ ABCheck.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ABCheck {
+    String ABCheck(String str) {
+        char[] chars = str.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == 'a'&& (i + 4) < chars.length && chars[i + 4] == 'b') {
+                return "true";
+            } else if (chars[i] == 'b'&& (i + 4) < chars.length && chars[i + 4] == 'a') {
+                return "true";
+            }
+        }
+
+        return "false";
+
+    }
+
+    public static void main (String[] args) {
+        Scanner s = new Scanner(System.in);
+        ABCheck c = new ABCheck();
+        System.out.print(c.ABCheck(s.nextLine()));
+    }
+}
